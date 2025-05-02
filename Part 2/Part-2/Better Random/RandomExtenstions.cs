84f5cc1647a274d7eb216a871096c43f455c30b6
Rory Martin <rory.martin@mhr.co.uk>
2025-05-02 16:31:15 +0100

    extension wip

---
+++ Part 2/Part-2/Better Random/RandomExtenstions.cs
@@ -0,0 +1,11 @@
+﻿namespace Better_Random;
+
+public static class RandomExtenstions
+{
+    public static double NextDouble(this Random random, double maxValue)
+    {
+        return random.NextDouble() * maxValue;
+    }
+    
+    
+}
